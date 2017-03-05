60cca1ac7f99b0537cfe225c3ef58cdad2c21f5f
Will Normand <nowayride@users.noreply.github.com>
2017-03-04 21:59:36 -0600

    Control all lights on /lights

--- src/app/lights/lights.component.ts
+++ src/app/lights/lights.component.ts
@@ -4,13 +4,25 @@ import { Http, Response } from '@angular/http';
 import { AuthService } from '../auth.service';
 import { AlertService } from '../alert.service';
 
+interface State {
+  bri: number
+  on?: boolean
+}
+
+interface Light {
+  id: number
+  name: string
+  state?: State
+}
+
 @Component({
   templateUrl: './lights.component.html'
 })
 
 export class LightsComponent implements OnInit {
-  private endpoint: string;
-  private lights: any;
+  private endpoint: string
+  private lights: Light[] = []
+  private updating: boolean = false
 
   constructor(
     private http: Http,
@@ -27,9 +39,47 @@ export class LightsComponent implements OnInit {
       })
       .subscribe(
       data => {
-        this.lights = data;
+        Object.keys(data).forEach(i => {
+          let light = data[i]
+          light.id = i
+          this.lights.push(light)
+        })
       },
       error => { this.alertService.danger(error) }
     )
+  }
+
+  trackBy(i, v) {
+    return i
+  }
+
+  changeBrightness(index: number, value: number) {
+    this.lights[index].state = {
+        on: +value > 0,
+        bri: +value
+      }
+    if(this.updating == true) { return }
+
+    this.updating = true
+
+    setTimeout(() => {
+      let state: State = this.lights[index].state
+      let id: number = this.lights[index].id
+
+      this.http.put(`${this.endpoint}/lights/${id}/state`, JSON.stringify(state)) 
+        .map((response: Response) => {
+          let data = response.json()
+          if('error' in data) {
+            throw new Error(data.error)
+          }
+        })
+        .subscribe(
+          data => {console.log(state)},
+          error => {
+            this.alertService.danger(error)
+          }
+        )
+      this.updating = false },
+      350)
   }
 }
