938501f04e416eade97c12ebea346dbf2818648c
Will Normand <nowayride@users.noreply.github.com>
2017-03-05 00:42:54 -0600

    Remove light detail route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,7 +3,6 @@ import { ModuleWithProviders } from '@angular/core';
 
 import { HomeComponent } from './home/home.component'
 import { LightsComponent } from './lights/lights.component'
-import { LightDetailComponent } from './lights/light-detail.component'
 import { RoomsComponent } from './rooms/rooms.component'
 import { ScenesComponent } from './scenes/scenes.component'
 import { SchedulesComponent } from './schedules/schedules.component'
@@ -12,7 +11,6 @@ import { HubsComponent } from './hubs/hubs.component'
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'lights', component: LightsComponent },
-  { path: 'lights/:id', component: LightDetailComponent },
   { path: 'rooms', component: RoomsComponent },
   { path: 'scenes', component: ScenesComponent },
   { path: 'schedules', component: SchedulesComponent },
