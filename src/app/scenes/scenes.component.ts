94f057435a2fd613dd978c00a3f660df3b889828
Will Normand <faheetah@users.noreply.github.com>
2017-04-18 16:02:30 -0500

    Preliminary schedules and scenes

--- src/app/scenes/scenes.component.ts
+++ src/app/scenes/scenes.component.ts
@@ -4,6 +4,17 @@ import { Http, Response } from '@angular/http';
 import { AuthService } from '../auth.service';
 import { AlertService } from '../alert.service';
 
+interface State {
+  bri: number
+  on?: boolean
+  reachable?: boolean
+}
+
+interface Light {
+  name: string
+  state?: State
+}
+
 @Component({
   templateUrl: './scenes.component.html',
 })
@@ -11,6 +22,7 @@ import { AlertService } from '../alert.service';
 export class ScenesComponent implements OnInit {
   private endpoint: string;
   private scenes: any;
+  private lights: {[key: string]: Light} = {}
 
   constructor(
     private http: Http,
@@ -21,16 +33,101 @@ export class ScenesComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getLights()
+    this.getScenes()
+  }
+
+  getLights() {
+    this.http.get(`${this.endpoint}/lights`)
+      .subscribe(
+      (response: Response) => {
+        let data = response.json()
+        Object.keys(data).forEach(i => {
+          let light = data[i]
+          if(!light.state.on) {
+            light.state.bri = 0
+          }
+        })
+        this.lights = data
+      },
+      error => { 
+        this.alertService.danger(error) 
+      }
+    )
+  }
+
+  getScenes() {
     this.http.get(this.endpoint + '/scenes')
+      .subscribe(
+      (response: Response) => {
+        let data = response.json()
+        Object.keys(data).forEach(scene => {
+          let lights: {[key: string]: Light} = {}
+          // probably better to abstract this out into a method to get all lights for a group
+          Object.keys(data[scene].lights).forEach(light => {
+            lights[data[scene].lights[light]] = this.lights[data[scene].lights[light]]
+          })
+          data[scene].lights = lights
+        })
+        this.scenes = data;
+      },
+      error => {
+        this.alertService.danger(error)
+      }
+    )
+  }
+
+  deleteScene(index: number) {
+    // if(!confirm('Really remove this scene?')) {
+      // return
+    // }
+    this.http.delete(`${this.endpoint}/scenes/${index}`)
       .map((response: Response) => {
-        return response.json()
+        let data = response.json()
+        if('error' in data) {
+          throw new Error(data.error)
+        }
       })
       .subscribe(
-        data => {
-          this.scenes = data;
+        () => {
+          this.alertService.danger(`Removed scene ${this.scenes[index]['name']}`)
+          this.getScenes()
         },
-        error => { this.alertService.danger(error) }
+        error => {
+          this.alertService.danger(error)
+        }
       )
   }
 
+
+  saveScene(index: string) {
+    let scene = {}
+    scene['name'] = this.scenes[index]['name']
+    scene['lights'] = Object.keys(this.scenes[index]['lights'])
+    // scene['storelightstate'] = true
+
+    this.http.put(`${this.endpoint}/scenes/${index}`, JSON.stringify(scene))
+      .map((response: Response) => {
+        let data = response.json()
+        data.forEach(msg => {
+            if('error' in msg) {
+              throw new Error(msg['error']['description'])
+            }
+          }
+        )
+      })
+      .subscribe(
+        () => {
+          this.alertService.success(`Updated scene ${this.scenes[index]['name']}`)
+          this.getScenes()
+        },
+        error => {
+          this.alertService.danger(error)
+        }
+      )
+  }
+
+  delete(obj: any, key: string) {
+    delete obj[key]
+  }
 }
